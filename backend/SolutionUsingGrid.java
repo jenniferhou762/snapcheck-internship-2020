0f4d5c1ab0d5ae387ac3c3a78ea61b6dec56e3fc
Lijuan Hou <lijuan.hou@sjsu.edu>
2020-04-29 16:19:17 -0700

    Add solution using grid

--- backend/SolutionUsingGrid.java
+++ backend/SolutionUsingGrid.java
@@ -1,24 +1,23 @@
 import java.util.*;
 import java.lang.Math;
 
-public class Solution {
+public class SolutionUsingGrid {
     public static String encryption(String s){
-        // As required: s is comprised only of characters in the range ascii[a-z].
+        // From description: s only has lower case characters
         // s = s.replace(" ", "");
 
-        // But if s contains Upper cases and special characters, it works in this way.
+        // But if s contains Upper cases and Special characters, it works in this way
         s = s.replaceAll("[^a-zA-Z]", "");
         s = s.toLowerCase();
 
         int len = s.length();
-        // actually this is unnecessary, but can shorten the process in this case
-        if(len==0) {
+        if(len==0){
             return "";
         }
 
         int row = (int)(Math.sqrt(len));
         int col = row;
-        if(row * row < len) {
+        if(row * row < len){
             col++;
         }
         // ensures that row * col >= len;
@@ -26,22 +25,25 @@ public class Solution {
             row++;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for(int j=0; j<col; j++){
-            int i = j;
-            while(i < len){
-                char ch = s.charAt(i);
-                sb.append(ch);
-                i += col;
+        char[][] grid = new char[row][col];
+        int t = 0;
+        for(int r=0; r<row; r++){
+            for(int c=0; c<col && t<len; c++){
+                grid[r][c] = s.charAt(t++);
             }
-            sb.append(' ');
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<col; i++){
+            for(int j=0; j<row; j++){
+                sb.append(grid[j][i]);
+            }
+            sb.append(" ");
         }
         return sb.toString();
     }
 
     public static void main(String[] args){
-        // case s1 - s12: only lower cases and spaces
-        // case s13 - s14: ++ upper cases and special characters
         String s1 = "have a nice day";
         System.out.println(encryption(s1));
 
@@ -51,7 +53,7 @@ public class Solution {
         String s3 = "chill out";
         System.out.println(encryption(s3));
 
-        String s4 = "if man  was meant to stay on the ground god would have given us roots";
+        String s4 = "if man was meant to stay on the ground god would have given us roots";
         System.out.println(encryption(s4));
 
         String s5 = "there is nothing i can do for you";
@@ -66,23 +68,22 @@ public class Solution {
         String s8 = "aaaaaaaaaaaa";
         System.out.println(encryption(s8));
 
-        String s9 = "    abs   de  ";
+        String s9 = "     abs    de  ";
         System.out.println(encryption(s9));
 
-        String s10 = "a b";
+        String s10 = "a  b";
         System.out.println(encryption(s10));
 
-        String s11 = "a   ";
+        String s11 = "a          ";
         System.out.println(encryption(s11));
 
-        String s12 = "        ";
+        String s12 = "            ";
         System.out.println(encryption(s12));
 
-        String s13 = "  @@@  can you imagine this?!!!";
+        String s13 = "   #$#@ can you imagine this ?! *&";
         System.out.println(encryption(s13));
 
-        String s14 = "   YOU deserve this!!!";
+        String s14 = "    YOU deserve This!!!!!!  :)";
         System.out.println(encryption(s14));
-
     }
 }
